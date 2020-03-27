2aaab0ec7ff7a85c4ecf2550c9b2632e7536de6d
Jee Yoon Kim <jk2348@cornell.edu>
2020-03-27 14:57:13 +0900

    resolved multiple toggle & slideshow

--- script/toggle.js
+++ script/toggle.js
@@ -1,14 +1,9 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
+$('#toggle-control').click(function(){
+    if($('#target').hasClass("open")){
+        $('#target').css("display","none");
+        $('#target').removeClass("open");
+    } else{
+        $('#target').css("dispaly", "block");
+        $('#target').addClass("open");
     }
-  });
-}
+});
