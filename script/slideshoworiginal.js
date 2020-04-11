c868bc613a97906cd0eda4bd4f6c833a8d38ed40
Jee Yoon Kim <jk2348@cornell.edu>
2020-04-12 04:58:27 +0900

    CURE FINAL as of today

--- script/slideshoworiginal.js
+++ script/slideshoworiginal.js
@@ -14,10 +14,21 @@ function currentSlide(n, no) {
 function showSlides(n, no) {
   var i;
   var x = document.getElementsByClassName(slideId[no]);
+  var dots = document.getElementsByClassName("dot");
+
   if (n > x.length) {slideIndex[no] = 1}    
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
-  x[slideIndex[no]-1].style.display = "block";  
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+
+x[slideIndex[no]-1].style.display = "block";  
+dots[slideIndex[no]-1].className += " active";
+
+
+  
 }
